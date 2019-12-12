c938decc42e08dc84987d86d1fb92b9da1a5018c
Johnny <jo@gmail.com>
2019-12-13 01:35:14 +0800

    add package:html

--- lib/Fetch.dart
+++ lib/Fetch.dart
@@ -1,17 +1,22 @@
 import 'package:http/http.dart' as http;
 import 'package:flutter/physics.dart';
 import 'package:flutter/material.dart';
+import 'package:html/parser.dart' show parse;
+import 'package:html/dom.dart';
+
 
 class Fetch {
 
   static void handleData() async{
     String result = await fetchPost();
-    print(result);
+    Document document = parse(result);
+    var aa = document.querySelector('ul#36HR_MOD');
+    print(aa);
   }
 
   static Future<String> fetchPost() async {
-    await Future.delayed(Duration(seconds: 3));
-    String url = "https://dart.dev/codelabs/async-await";
+//    await Future.delayed(Duration(seconds: 3));
+    String url = "https://www.cwb.gov.tw/V8/C/W/County/County.html?CID=63";
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
